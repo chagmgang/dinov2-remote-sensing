6753f2a3f294def0df73c5050e736a7cd87206b2
root <chagmgang@gmail.com>
2024-12-23 08:51:12 +0000

    notebook and setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='dinov2',
+    version='0.0.1',
+    author='ChaKeumgang',
+    author_email='chagmgang@gmail.com',
+    include_package_data=False,
+    packages=setuptools.find_packages(),
+)
